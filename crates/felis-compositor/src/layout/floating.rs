dd4b9c7b9460d013d154cecd1ef9fbc54b7cecdc
lemonade-2061 <lemonade.2061.7.29@gmail.com>
2026-05-29 04:23:45 +0900

    feat:floatinglayout

--- crates/felis-compositor/src/layout/floating.rs
+++ crates/felis-compositor/src/layout/floating.rs
@@ -1,28 +1,39 @@
-use crate::layout::Horizontal;
+use crate::layout::{Direction, WindowNav};
 use smithay::desktop::Window;
 pub struct FloatingLayout {
     windows: Vec<Window>,
+    focused: Option<usize>,
 }
 
 impl FloatingLayout {
     pub fn new() -> Self {
         Self {
             windows: Vec::new(),
+            focused: Option::None,
         }
     }
 }
 
-impl Horizontal for FloatingLayout {
+impl WindowNav for FloatingLayout {
     fn focus(&mut self, dir: Direction) -> Option<Window> {
-        let sign = match dir {
-            Direction::Right => 1,
-            Direction::Left => -1,
+        let cur = self.focused?;
+
+        let next = match dir {
+            Direction::Right => cur + 1,
+            Direction::Left => cur.checked_sub(1)?,
         };
+
+        if next >= self.windows.len() {
+            return Some(self.windows[0].clone());
+        }
+
+        self.focused = Some(next);
+        Some(self.windows[next].clone())
     }
-    fn move_window(&mut self, dir: super::Direction) -> bool {
-        todo!()
+    fn move_window(&mut self, _dir: super::Direction) -> bool {
+        false
     }
-    fn resize(&mut self, dir: super::Direction, delta: i32) -> bool {
-        todo!()
+    fn resize(&mut self, _dir: super::Direction, _delta: i32) -> bool {
+        false
     }
 }
