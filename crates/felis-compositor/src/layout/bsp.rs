ec61c00300585e45f10deead374b8227b8781c37
lemonade-2061 <lemonade.2061.7.29@gmail.com>
2026-05-31 03:29:31 +0900

    途中

--- crates/felis-compositor/src/layout/bsp.rs
+++ crates/felis-compositor/src/layout/bsp.rs
@@ -1,14 +1,35 @@
 use crate::layout::{Direction, WindowNav};
 use smithay::desktop::Window;
 
+type Node = usize;
 pub struct BspLayout {
-    todo!()
+    nodes: Vec<Option<Node>>,
+    free_list: Vec<usize>,
+    root: Option<usize>,
+    focused: Option<usize>,
 }
 
 impl BspLayout {
     fn new() -> Self {
         Self {
-
+            nodes: Vec::new(),
+            free_list: Vec::new(),
+            root: None,
+            focused: None,
         }
+    }
+}
+
+impl WindowNav for BspLayout {
+    fn focus(&mut self, dir: Direction) -> Option<Window> {
+        todo!()
+    }
+
+    fn move_window(&mut self, dir: Direction) -> bool {
+        todo!()
+    }
+
+    fn resize(&mut self, dir: Direction, delta: i32) -> bool {
+        todo!()
     }
 }
