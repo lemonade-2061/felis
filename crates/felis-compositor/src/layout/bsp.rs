7a0746e67c7bdc12adfca4180b50e735e1728796
lemonade-2061 <lemonenergy.2061.7.29@gmail.com>
2026-06-26 19:03:59 +0900

    BSP-add

--- crates/felis-compositor/src/layout/bsp.rs
+++ crates/felis-compositor/src/layout/bsp.rs
@@ -1,12 +1,36 @@
 use crate::layout::{Direction, WindowNav};
 use smithay::desktop::Window;
 
-type Node = usize;
-pub struct BspLayout {
-    nodes: Vec<Option<Node>>,
-    free_list: Vec<usize>,
-    root: Option<usize>,
-    focused: Option<usize>,
+type NodeId = usize;
+
+enum Axis {
+    Horizontal,
+    Vertical,
+}
+
+enum NodeKind {
+    Leaf {
+        window: Window,
+    },
+
+    Split {
+        axis: Axis,
+        ratio: f32,
+        first: NodeId,
+        second: NodeId,
+    },
+}
+
+struct BspNode {
+    kind: NodeKind,
+    parent: Option<NodeId>,
+}
+
+struct BspLayout {
+    nodes: Vec<Option<BspNode>>,
+    free_list: Vec<NodeId>,
+    root: Option<NodeId>,
+    focused: Option<NodeId>,
 }
 
 impl BspLayout {
@@ -18,9 +42,57 @@ impl BspLayout {
             focused: None,
         }
     }
+
+    fn alloc(&mut self, kind: NodeKind) -> NodeId {
+        let node = BspNode { kind, parent: None };
+
+        match self.free_list.pop() {
+            Some(id) => {
+                self.nodes[id] = Some(node);
+                id
+            }
+
+            None => {
+                self.nodes.push(Some(node));
+                self.nodes.len() - 1
+            }
+        }
+    }
 }
 
 impl WindowNav for BspLayout {
+    fn add(&mut self, window: Window) {
+        let new_leaf = self.alloc(NodeKind::Leaf { window });
+
+        match self.focused {
+            None => {
+                self.root = Some(new_leaf);
+            }
+            Some(target) => {
+                let window = match &self.nodes[target] {
+                    Some(BspNode {
+                        kind: NodeKind::Leaf { window },
+                        ..
+                    }) => window.clone(),
+                    _ => unreachable!("focusedは葉のはず"),
+                };
+
+                let old_leaf = self.alloc(NodeKind::Leaf { window });
+                let node = self.nodes[target].as_mut().unwrap();
+
+                node.kind = NodeKind::Split {
+                    axis: Axis::Horizontal,
+                    ratio: 0.5,
+                    first: old_leaf,
+                    second: new_leaf,
+                };
+
+                self.nodes[old_leaf].as_mut().unwrap().parent = Some(target);
+                self.nodes[new_leaf].as_mut().unwrap().parent = Some(target);
+            }
+        }
+        self.focused = Some(new_leaf);
+    }
     fn focus(&mut self, dir: Direction) -> Option<Window> {
         todo!()
     }
